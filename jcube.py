184147d85b251bcaf2b79f3d37808e06b8cfeea8
Jakub Kotur <jakub.kotur@gmail.com>
2011-11-24 18:12:43 +0100

    Jelly simulation
    + added code that calculates springs between points
    + added springs between points
    + for testing purpose gravity works only for 4 points

--- jcube.py
+++ jcube.py
@@ -2,24 +2,35 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+import math as m
 import numpy as np
+import random as rnd
 
 import cjelly
 from drawable import Drawable
 
 class JellyCube( Drawable ) :
 	SHAPE = (4,4,4)
-	C = 1.0
-	K = 1.0
+	C = 0.01
+	K = 2.0
 
 	def __init__( self ) :
 		self.pts = np.zeros( (self.SHAPE[0],self.SHAPE[1],self.SHAPE[2],3) , np.float64 )
 		self.prv = np.zeros( (self.SHAPE[0],self.SHAPE[1],self.SHAPE[2],3) , np.float64 )
-		self.frs = np.zeros( (self.SHAPE[0],self.SHAPE[1],self.SHAPE[2],3) , np.float64 )
+		self.pl = np.zeros( (self.SHAPE[0],self.SHAPE[1],self.SHAPE[2],3) , np.float64 )
+		self.nl = np.zeros( (self.SHAPE[0],self.SHAPE[1],self.SHAPE[2],3) , np.float64 )
 		self.mas = np.ones ( (self.SHAPE[0],self.SHAPE[1],self.SHAPE[2],3) , np.float64 )
+		self.l0 = 1
+		self.l1 = m.sqrt(2) * self.l0
 
-		self.__pts_aligned( 1 )
-		self.__prv_diff( 0 )
+		self.__pts_aligned( self.l0 )
+		self.__func( self.prv , lambda x,y,z : self.pts[x,y,z] )
+		self.__func( self.pl  , lambda x,y,z : 0 )
+		self.__func( self.mas , lambda x,y,z : 10 )
+#        self.pts[2,3,2,1] += .5
+#        self.prv[2,3,2,1] += .5
+
+		self.t = -3
 
 	def __pts_aligned( self , s ) :
 		for x in range(self.SHAPE[0]) :
@@ -27,26 +38,30 @@ class JellyCube( Drawable ) :
 				for z in range(self.SHAPE[2]) :
 					self.pts[x,y,z] = np.array((x,y,z)) * s
 
-	def __prv_diff( self , df ) :
+	def __func( self , p , f ) :
 		for x in range(self.SHAPE[0]) :
 			for y in range(self.SHAPE[1]) :
 				for z in range(self.SHAPE[2]) :
-					self.prv[x,y,z] = self.pts[x,y,z] + df
+					p[x,y,z] = f( x,y,z )
 
 	def gfx_init( self ) :
 		pass
 
 	def draw( self ) :
-		glPointSize( 2 )
-		glColor3f(0.3,1.0,0)
+		glPointSize( 5 )
 		glBegin(GL_POINTS)
 		for x in range(self.SHAPE[0]) :
 			for y in range(self.SHAPE[1]) :
 				for z in range(self.SHAPE[2]) :
+					glColor3f(x/4.0 , y/4.0 , z/4.0 )
 					glVertex3f( *self.pts[x,y,z] )
 		glEnd()
 
 
 	def wobble( self , dt ) :
-		cjelly.wobble( self.pts , self.prv , self.frs , self.mas , dt )
+		self.t += dt 
+		if self.t < 0 : return 
+
+		cjelly.springs( self.pts , self.nl , self.l0 , self.l1 )
+		cjelly.update( self.pts , self.prv , self.pl , self.nl , self.mas , self.K ,self.C , dt )
 
--- jelly/jelly.pyx
+++ jelly/jelly.pyx
@@ -8,21 +8,23 @@ import math as m
 
 import numpy as np
 
+
 cdef float Gx = 0
 cdef float Gy =-9
 cdef float Gz = 0
 
 #@cython.boundscheck(False)
-cpdef int wobble( np.ndarray[ double , ndim = 4 ] pts ,
-                  np.ndarray[ double , ndim = 4 ] prv ,
-                  np.ndarray[ double , ndim = 4 ] frs ,
-                  np.ndarray[ double , ndim = 4 ] mas ,
-                  double dt ) :
-	springs( pts , frs )
-	update ( pts , prv , frs , mas , dt )
+cpdef np.ndarray[ double ] spring_force( np.ndarray[ double ] p0 ,
+                        np.ndarray[ double ] p1 ,
+						double l0 ) :
+	cdef np.ndarray[ double ] dp = p1 - p0
+	cdef double dl = np.linalg.norm( dp )
+	cdef double l = l0 - dl
+	return dp / dl * l
 
 cpdef int springs( np.ndarray[ double , ndim = 4 ] pts ,
-                   np.ndarray[ double , ndim = 4 ] frs ) :
+                   np.ndarray[ double , ndim = 4 ] nl , 
+				   double l0 , double l1 ) :
 	cdef int x , y , z
 
 	x = 0
@@ -31,9 +33,46 @@ cpdef int springs( np.ndarray[ double , ndim = 4 ] pts ,
 		while y < pts.shape[1] :
 			z = 0
 			while z < pts.shape[2] :
-				frs[x,y,z,0] = Gx
-				frs[x,y,z,1] = Gy 
-				frs[x,y,z,2] = Gz
+				nl[x,y,z,0] = 0
+				nl[x,y,z,1] = 0
+				nl[x,y,z,2] = 0
+
+				if x+1 < pts.shape[0] :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x+1,y,z],l0)
+				if y+1 < pts.shape[1] :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x,y+1,z],l0)
+				if z+1 < pts.shape[2] :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x,y,z+1],l0)
+				if x-1 >= 0 :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x-1,y,z],l0)
+				if y-1 >= 0 :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x,y-1,z],l0)
+				if z-1 >= 0 :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x,y,z-1],l0)
+				if x+1 < pts.shape[0] and y+1 < pts.shape[1] :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x+1,y+1,z],l1)
+				if y+1 < pts.shape[1] and z+1 < pts.shape[2] :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x,y+1,z+1],l1)
+				if z+1 < pts.shape[2] and x+1 < pts.shape[0] :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x+1,y,z+1],l1)
+				if x-1 >= 0 and y+1 < pts.shape[1] :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x-1,y+1,z],l1)
+				if y-1 >= 0 and z+1 < pts.shape[2] :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x,y-1,z+1],l1)
+				if z-1 >= 0 and x+1 < pts.shape[0] :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x+1,y,z-1],l1)
+				if x+1 < pts.shape[0] and y-1 >= 0 :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x+1,y-1,z],l1)
+				if y+1 < pts.shape[1] and z-1 >= 0 :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x,y+1,z-1],l1)
+				if z+1 < pts.shape[2] and x-1 >= 0 :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x-1,y,z+1],l1)
+				if x-1 >= 0 and y-1 >= 0 :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x-1,y-1,z],l1)
+				if y-1 >= 0 and z-1 >= 0 :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x,y-1,z-1],l1)
+				if z-1 >= 0 and x-1 >= 0 :
+					nl[x,y,z]+=spring_force(pts[x,y,z],pts[x-1,y,z-1],l1)
 				z += 1
 			y += 1
 		x += 1
@@ -41,8 +80,10 @@ cpdef int springs( np.ndarray[ double , ndim = 4 ] pts ,
 
 cpdef int update( np.ndarray[ double , ndim = 4 ] pts ,
                   np.ndarray[ double , ndim = 4 ] prv ,
-                  np.ndarray[ double , ndim = 4 ] frs ,
+                  np.ndarray[ double , ndim = 4 ] pl  ,
+                  np.ndarray[ double , ndim = 4 ] nl  ,
                   np.ndarray[ double , ndim = 4 ] mas ,
+				  double k , double c ,
                   double dt ) :
 	cdef np.ndarray a = np.zeros( 3 )
 	cdef np.ndarray v = np.zeros( 3 )
@@ -55,9 +96,12 @@ cpdef int update( np.ndarray[ double , ndim = 4 ] pts ,
 		while y < pts.shape[1] :
 			z = 0
 			while z < pts.shape[2] :
-				a = frs[x,y,z] / mas[x,y,z]
+				dl  = ( nl[x,y,z] - pl[x,y,z] ) / ( 2 * dt )
+				f = -k * dl - c * nl[x,y,z]
+				if y == 3 and x >= 1 and x <=2 and z >= 1 and z <= 2 :
+					f[1] -= 9
+				a = f / mas[x,y,z]
 				n = a * dt * dt + 2 * pts[x,y,z] - prv[x,y,z]
-				v  = ( n - prv[x,y,z] ) / ( 2 * dt )
 				prv[x,y,z] = pts[x,y,z]
 				pts[x,y,z] = n
 				z += 1
