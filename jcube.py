6b74dfaee201be2b9de0d4a8c5509b85e4c29666
Jakub Kotur <jakub.kotur@gmail.com>
2011-11-24 16:59:22 +0100

    Falling cube
    + added physical simulation based on three-point estimation
      differentiation model

--- jcube.py
+++ jcube.py
@@ -11,17 +11,27 @@ class JellyCube( Drawable ) :
 	SHAPE = (4,4,4)
 	C = 1.0
 	K = 1.0
-	L0 = .1
 
 	def __init__( self ) :
 		self.pts = np.zeros( (self.SHAPE[0],self.SHAPE[1],self.SHAPE[2],3) , np.float64 )
-		self.__init_aligned()
+		self.prv = np.zeros( (self.SHAPE[0],self.SHAPE[1],self.SHAPE[2],3) , np.float64 )
+		self.frs = np.zeros( (self.SHAPE[0],self.SHAPE[1],self.SHAPE[2],3) , np.float64 )
+		self.mas = np.ones ( (self.SHAPE[0],self.SHAPE[1],self.SHAPE[2],3) , np.float64 )
 
-	def __init_aligned( self ) :
+		self.__pts_aligned( 1 )
+		self.__prv_diff( 0 )
+
+	def __pts_aligned( self , s ) :
 		for x in range(self.SHAPE[0]) :
 			for y in range(self.SHAPE[1]) :
 				for z in range(self.SHAPE[2]) :
-					self.pts[x,y,z] = np.array((x,y,z))
+					self.pts[x,y,z] = np.array((x,y,z)) * s
+
+	def __prv_diff( self , df ) :
+		for x in range(self.SHAPE[0]) :
+			for y in range(self.SHAPE[1]) :
+				for z in range(self.SHAPE[2]) :
+					self.prv[x,y,z] = self.pts[x,y,z] + df
 
 	def gfx_init( self ) :
 		pass
@@ -38,5 +48,5 @@ class JellyCube( Drawable ) :
 
 
 	def wobble( self , dt ) :
-		cjelly.wobble( self.pts , dt )
+		cjelly.wobble( self.pts , self.prv , self.frs , self.mas , dt )
 
--- jelly/jelly.pyx
+++ jelly/jelly.pyx
@@ -13,6 +13,54 @@ cdef float Gy =-9
 cdef float Gz = 0
 
 #@cython.boundscheck(False)
-cpdef int wobble( np.ndarray[ double , ndim = 4 ] pts , double dt ) :
-	return 0
+cpdef int wobble( np.ndarray[ double , ndim = 4 ] pts ,
+                  np.ndarray[ double , ndim = 4 ] prv ,
+                  np.ndarray[ double , ndim = 4 ] frs ,
+                  np.ndarray[ double , ndim = 4 ] mas ,
+                  double dt ) :
+	springs( pts , frs )
+	update ( pts , prv , frs , mas , dt )
+
+cpdef int springs( np.ndarray[ double , ndim = 4 ] pts ,
+                   np.ndarray[ double , ndim = 4 ] frs ) :
+	cdef int x , y , z
+
+	x = 0
+	while x < pts.shape[0] :
+		y = 0
+		while y < pts.shape[1] :
+			z = 0
+			while z < pts.shape[2] :
+				frs[x,y,z,0] = Gx
+				frs[x,y,z,1] = Gy 
+				frs[x,y,z,2] = Gz
+				z += 1
+			y += 1
+		x += 1
+
+
+cpdef int update( np.ndarray[ double , ndim = 4 ] pts ,
+                  np.ndarray[ double , ndim = 4 ] prv ,
+                  np.ndarray[ double , ndim = 4 ] frs ,
+                  np.ndarray[ double , ndim = 4 ] mas ,
+                  double dt ) :
+	cdef np.ndarray a = np.zeros( 3 )
+	cdef np.ndarray v = np.zeros( 3 )
+	cdef np.ndarray n = np.zeros( 3 )
+
+	cdef int x , y , z
+	x = 0
+	while x < pts.shape[0] :
+		y = 0
+		while y < pts.shape[1] :
+			z = 0
+			while z < pts.shape[2] :
+				a = frs[x,y,z] / mas[x,y,z]
+				n = a * dt * dt + 2 * pts[x,y,z] - prv[x,y,z]
+				v  = ( n - prv[x,y,z] ) / ( 2 * dt )
+				prv[x,y,z] = pts[x,y,z]
+				pts[x,y,z] = n
+				z += 1
+			y += 1
+		x += 1
 
